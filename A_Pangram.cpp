a54a870a89ea11bdc91c704d3d608939153d45bd
Md Ifti <mdifti404@gmail.com>
2025-08-02 18:03:25 +0600

    .

--- A_Pangram.cpp
+++ A_Pangram.cpp
@@ -1,27 +1,30 @@
 //	520A - Pangram
- 
+
 #include <iostream>
 #include <set>
 using namespace std;
- 
-int main() {
+
+int main()
+{
     int len;
     string s;
     set<char> S;
     cin >> len;
     cin >> s;
- 
-    for (int i = 0; i < len; i++) {
-        if (s[i] >= 'A' && s[i] <= 'Z') {
+
+    for (int i = 0; i < len; i++)
+    {
+        if (s[i] >= 'A' && s[i] <= 'Z')
+        {
             s[i] += 'a' - 'A';
         }
         S.insert(s[i]);
     }
- 
+
     if (S.size() == 26)
         cout << "YES" << endl;
     else
         cout << "NO" << endl;
- 
+
     return 0;
 }
