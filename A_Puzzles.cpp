b3c766571abbfc7248e4664f3c1ab5229409e614
Md <mdifti404@gmail.com>
2025-08-15 16:05:38 +0600

    .

---
+++ A_Puzzles.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n, m, result = 100000000;
+    cin >> n >> m;
+    vector<int> s(m);
+    for (int i = 0; i < m; i++)
+    {
+        cin >> s[i];
+    }
+
+    sort(s.begin(), s.end());
+
+    result = s[+n - 1] - f[k]
+}
